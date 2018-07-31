f74a4006f02afb7cc5d1a3fbce7ca2c0ecb30e1b
aleclarson <alec.stanford.larson@gmail.com>
2018-07-31 19:44:40 -0400

    use @aleclarson/inquirer

--- src/node-plop.js
+++ src/node-plop.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import path from 'path';
-import inquirer from 'inquirer';
+import inquirer from '@aleclarson/inquirer';
 import handlebars from 'handlebars';
 import _get from 'lodash.get';
 import resolve from 'resolve';
