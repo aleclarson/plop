3ab5d9b25bcd41668c19a0b05a50005779e4b8c8
aleclarson <alec.stanford.larson@gmail.com>
2018-08-01 00:00:35 -0400

    fix: throw checkInterface failures

--- src/actions/_common-action-interface-check.js
+++ src/actions/_common-action-interface-check.js
@@ -2,18 +2,18 @@ export default function(action, {checkPath=true, checkAbortOnFail=true} = {}) {
 
 	// it's not even an object, you fail!
 	if (typeof action !== 'object') {
-		return `Invalid action object: ${JSON.stringify(action)}`;
+		throw Error(`Invalid action object: ${JSON.stringify(action)}`);
 	}
 
 	const {path, abortOnFail} = action;
 
 	if (checkPath && (typeof path !== 'string' || path.length === 0)) {
-		return `Invalid path "${path}"`;
+		throw Error(`Invalid path "${path}"`);
 	}
 
 	// abortOnFail is optional, but if it's provided it needs to be a Boolean
 	if (checkAbortOnFail && abortOnFail !== undefined && typeof abortOnFail !== 'boolean') {
-		return `Invalid value for abortOnFail (${abortOnFail} is not a Boolean)`;
+		throw Error(`Invalid value for abortOnFail (${abortOnFail} is not a Boolean)`);
 	}
 
 	return true;
